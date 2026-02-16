dae478c1def0e8057f858f648a4b375f16fd7c68
DYNAMIC_DISHANK <dishankpatel16082003@gmail.com>
2026-02-16 12:24:54 +0530

    Setup for Employees

---
+++ section-4-spring-boot/employees/src/main/java/com/dishank/springboot/employees/EmployeesForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dishank.springboot.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesForSpringBootApplication.class, args);
+	}
+
+}
---
+++ section-4-spring-boot/employees/src/test/java/com/dishank/springboot/employees/EmployeesForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishank.springboot.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
