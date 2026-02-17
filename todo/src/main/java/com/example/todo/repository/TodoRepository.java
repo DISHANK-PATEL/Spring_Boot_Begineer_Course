8c60a4c9e00107ea6af896fda587e29246a87c79
DYNAMIC_DISHANK <dishankpatel16082003@gmail.com>
2026-02-17 21:41:47 +0530

    Added the todo and user repository

---
+++ todo/src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import com.example.todo.entity.Todo;
+import com.example.todo.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+    List<Todo> findByOwner(User owner);
+    Optional<Todo> findByIdAndOwner(Long id, User owner);
+}
