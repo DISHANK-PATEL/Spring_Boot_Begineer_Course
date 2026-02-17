fb9fb762ca88caf689f32f401308aa78f56e3b9d
DYNAMIC_DISHANK <dishankpatel16082003@gmail.com>
2026-02-17 17:42:26 +0530

    Implemented the services related to Jwt and user

---
+++ todo/src/main/java/com/example/todo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.todo.service;
+
+import com.example.todo.request.PasswordUpdateRequest;
+import com.example.todo.response.UserResponse;
+
+public interface UserService {
+    UserResponse getUserInfo();
+    void deleteUser();
+    void updatePassword(PasswordUpdateRequest passwordUpdateRequest);
+}
